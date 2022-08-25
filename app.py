34f9dff2f11ea704a3be817f37b4a91a12eef4c0
tenjin <103417420+tenjinspace@users.noreply.github.com>
2022-08-25 20:05:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import requests as r
+import pandas as pd
+import json
 
+baseUrl = "https://extranet.who.int/publicemergency/api/Monkeypox/CaseSummary"
+rawData = r.post(baseUrl)
+rawData = rawData.json()
+
+df = pd.DataFrame(rawData["Data"])
+st.dataframe(df)
