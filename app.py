407997ea9e9c5548320e39181b3688fffd39937c
Andrea V <33574513+andvg@users.noreply.github.com>
2022-08-27 11:53:31 +0200

    dev

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ import requests as r
 import pandas as pd
 import json
 
+st.title('My title')
+st.header('My header')
+st.subheader('My sub')
+
 baseUrl = "https://extranet.who.int/publicemergency/api/Monkeypox/CaseSummary"
 rawData = r.post(baseUrl)
 rawData = rawData.json()
 
 df = pd.DataFrame(rawData["Data"])
-st.table(rawData)
 st.dataframe(df)
