d8207958a6c767114c917e58bcb787cf7d5e470e
Andrea V <33574513+andvg@users.noreply.github.com>
2022-08-27 13:01:41 +0200

    dev

--- app.py
+++ app.py
@@ -2,22 +2,33 @@ import streamlit as st
 import requests as r
 import pandas as pd
 import json
+from datetime import date
 
-st.title('My title')
-st.header('My header')
-st.subheader('My sub')
+today = date.today()
+
+st.title('Moneypox data')
+st.header('by WHO Health Emergency Dashboard')
+st.subheader('Today is: {}'.format(today))
 
 baseUrl = "https://extranet.who.int/publicemergency/api/Monkeypox/CaseSummary"
-rawData = r.post(baseUrl)
-rawData = rawData.json()
 
-df = pd.DataFrame(rawData["Data"])
-df = df.replace(r'^s*$', float('NaN'), regex = True)
-newDf = df.dropna(axis=0, subset=['ISO3'])
+def getData(baseUrl):
+    rawData = r.post(baseUrl)
+    rawData = rawData.json()
+    return rawData
 
-st.json(rawData)
-st.dataframe(df)
-st.write(df.info())
-st.dataframe(newDf)
-text = newDf.info()
-st.write(text)
+def cleanData(rawData):
+    df = pd.DataFrame(rawData["Data"])
+    df = df.replace(r'^s*$', float('NaN'), regex = True)
+    newDf = df.dropna(axis=0, subset=['ISO3'])
+    return df, newDf
+
+def displayData(newDf):
+    st.dataframe(newDf)
+
+if st.button('Click to load data'):
+     rawData = getData(baseUrl)
+     data = cleanData(rawData)
+     displayData(data)
+ else:
+     st.write('Goodbye')
